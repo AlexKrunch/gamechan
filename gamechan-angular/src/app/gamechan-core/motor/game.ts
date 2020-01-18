f7fcf2ba3988d1a437131573b22d3ee6fab1b2ed
Alex Krunch <monsieur.krunch@gmail;com>
2020-01-18 14:37:48 +0100

    Scene running.

---
+++ gamechan-angular/src/app/gamechan-core/motor/game.ts
@@ -0,0 +1,87 @@
+
+
+import {
+    Engine, Scene, FreeCamera, Vector3, Mesh,
+    StandardMaterial, Texture,
+} from 'babylonjs'
+
+import {GameUtils} from './game-utils'
+
+export class Game {
+
+    //Singleton data
+    private static instance: Game;
+    private canvas: HTMLCanvasElement;
+    private engine: Engine;
+    private scene: Scene;
+    private gameUtils : GameUtils;
+
+    constructor(canvasElement : string) {
+
+        //Set the instance
+        Game.instance = this;
+        this.canvas = <HTMLCanvasElement> document.getElementById(canvasElement);
+        this.engine = new Engine(this.canvas, true, null, false);
+  
+        // Listen for browser/canvas resize events
+        window.addEventListener("resize", ()=> {
+          this.engine.resize();
+        });
+        
+    }
+
+    private playerHeight = 4; // The player eyes height
+    private speed = 1;
+    private inertia = 0.9;
+    private angularSensibility = 1000;
+
+
+    public initScene(){
+        
+        if( this.scene != null)  this.scene.dispose();
+        // We need a scene to create all our geometry and babylonjs items in
+        this.scene = new Scene(this.engine);
+        this.scene.audioEnabled = false;
+
+        //Camera FPS
+        let camera = new FreeCamera('freeCamera', new Vector3(0, 5,-10), this.scene);
+        camera.attachControl(this.canvas);
+        this.scene.gravity = new Vector3(0, -0.7, 0);
+        camera.applyGravity = true;
+
+        camera.ellipsoid = new Vector3(2.5, this.playerHeight, 2.5);
+        camera.ellipsoidOffset = new Vector3(0, this.playerHeight, 0);
+        camera.checkCollisions = true;
+            
+        GameUtils.setKeyBoardMapping(this);
+
+        camera.speed = this.speed;
+        camera.inertia = this.inertia;
+        camera.angularSensibility =this.angularSensibility;
+
+        //Add ground
+        let ground: Mesh;
+        ground = Mesh.CreateGround("ground", 1000, 1000, 2, this.scene);
+        ground.checkCollisions = true;
+        ground.position.y = - 0.1;
+    
+        let mat = new StandardMaterial("matVolcano", this.scene);
+        let texture = new Texture("./assets/textures/volcanic_text.jpg", this.scene);
+        mat.diffuseTexture = texture;
+        ground.material = mat;
+
+        //Launch teh run
+        this.run();
+
+    }
+
+    //Render process
+    run() : void {
+        this.engine.runRenderLoop(()=> {
+        if(this.scene != null){
+            this.scene.render();
+        }
+        });
+    }
+
+}
--- gamechan-angular/src/app/gamechan-core/views/game-view/game-view.component.ts
+++ gamechan-angular/src/app/gamechan-core/views/game-view/game-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../../motor/game';
 
 @Component({
   selector: 'app-game-view',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameViewComponent implements OnInit {
 
+  public game : Game;
+
   constructor() { }
 
   ngOnInit() {
+
+    // Create our game class using the render canvas element
+    this.game = new Game('renderCanvas');
+    this.game.initScene();
+
   }
 
 }
