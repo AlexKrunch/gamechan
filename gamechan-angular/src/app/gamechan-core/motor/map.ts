8d64d286e4e96803131a64ac07c9f335170bf18c
Alex Krunch <monsieur.krunch@gmail;com>
2020-01-19 13:21:49 +0100

    painting on wall work

--- gamechan-angular/src/app/gamechan-core/motor/map.ts
+++ gamechan-angular/src/app/gamechan-core/motor/map.ts
@@ -38,14 +38,38 @@ export class Map {
         this.scene.pointerMovePredicate = function(mesh) {
             return mesh.isPickable;
         }
+
+        this.scene.pointerUpPredicate = function(mesh) {
+            return mesh.isPickable;
+        }
     
         //When pointer down event is raised
         this.scene.onPointerMove = (evt, pickResult) => {
            if(pickResult.hit){
-                console.log(pickResult.pickedMesh.name);
+                //console.log(pickResult.pickedMesh.name);
                 if(pickResult.pickedMesh.name.indexOf('ground')>-1) this.moveGhostMesh(pickResult.pickedPoint);
            }
         };
+
+        this.scene.onPointerUp = (evt, pickResult) => {
+            if(pickResult.hit){
+                 console.log("Make block");
+                 if(pickResult.pickedMesh.name.indexOf('ground')>-1){
+
+                    this.makeBlock(pickResult.pickedPoint.x,pickResult.pickedPoint.z,'./assets/textures/concrete_text.jpg');
+
+                 } else if(pickResult.pickedMesh.name.indexOf('wall')>-1){
+                    let mesh = pickResult.pickedMesh;
+
+                    let mat = new StandardMaterial("matGround", this.scene);
+                    //let textureBox = new Texture('https://pbs.twimg.com/media/EOoDty5XkAANDcZ?format=jpg&name=small', this.scene);
+                    let textureBox = new Texture('https://pbs.twimg.com/media/EOpI7_3WsAA1hkU?format=jpg&name=360x360', this.scene);
+                    mat.diffuseTexture = textureBox;
+                    mat.diffuseTexture.scale(1/4) ;
+                    mesh.material = mat;
+                 }
+            }
+         };
 
     }
 
@@ -55,16 +79,16 @@ export class Map {
         let z = 0;
         //Create the walls
         //N
-        for(x = 0; x < this.mapWidth; x++) this.makeBlock(x,z,'./assets/textures/concrete_text.jpg');
+        for(x = 0; x < this.mapWidth; x++) this.makeBlock(x* this.blockSize,z* this.blockSize,'./assets/textures/concrete_text.jpg');
 
         z =  this.mapHeight;
-        for(x = 0; x < this.mapWidth; x++) this.makeBlock(x,z,'./assets/textures/concrete_text.jpg');
+        for(x = 0; x < this.mapWidth; x++) this.makeBlock(x* this.blockSize,z* this.blockSize,'./assets/textures/concrete_text.jpg');
 
         x = 0;
-        for(z = 0; z < this.mapHeight; z++) this.makeBlock(x,z,'./assets/textures/concrete_text.jpg');
+        for(z = 0; z < this.mapHeight; z++) this.makeBlock(x* this.blockSize,z* this.blockSize,'./assets/textures/concrete_text.jpg');
 
         x = this.mapWidth;
-        for(z = 0; z < this.mapHeight; z++) this.makeBlock(x,z,'./assets/textures/concrete_text.jpg');
+        for(z = 0; z < this.mapHeight; z++) this.makeBlock(x* this.blockSize,z* this.blockSize,'./assets/textures/concrete_text.jpg');
 
     }
 
@@ -79,7 +103,7 @@ export class Map {
     private makeBlock(x_,z_,text_){
 
         let mesh : Mesh = MeshBuilder.CreateBox("wall_tile", {size: this.blockSize},  this.scene);
-        mesh.checkCollisions = true;
+        //mesh.checkCollisions = true;
         mesh.isPickable = true;
         let mat = new StandardMaterial("matGround", this.scene);
         let textureBox = new Texture(text_, this.scene);
@@ -87,8 +111,8 @@ export class Map {
         mat.diffuseTexture.scale(1/4) ;
         mesh.material = mat;
 
-        mesh.position.x = x_ * this.blockSize;
-        mesh.position.z = z_ * this.blockSize;
+        mesh.position.x = x_;
+        mesh.position.z = z_;
         mesh.position.y = this.blockSize * 0.5;
 
     }
@@ -97,7 +121,6 @@ export class Map {
         pos_.y = pos_.y + (this.blockSize * 0.5);
         pos_.x = Math.round(pos_.x  / this.blockSize)*this.blockSize;
         pos_.z = Math.round(pos_.z  / this.blockSize)*this.blockSize;
-
         this.ghostMesh.position = pos_;
     }
 }
